5d0249f03bf5c7ad488f150ef667b48d2581ae3f
laceous <laceous@gmail.com>
2023-02-19 16:17:55 -0800

    add to github

---
+++ remove mothers invisible wall/main.lua
@@ -0,0 +1,50 @@
+local mod = RegisterMod('Remove Mother\'s Invisible Wall', 1)
+local game = Game()
+
+mod.gridIndexes = {}
+
+-- invisible blocks show up as walls in onNewRoom
+-- you can remove walls in onNewRoom but you can walk out of frame below the camera view
+-- change invisible blocks to cobwebs and remove them in onNewRoom
+-- the camera will work properly in this case
+-- entity types are taken from basement renovator
+function mod:onPreNewRoom(entityType, variant, subType, gridIdx, seed)
+  local room = game:GetRoom()
+  
+  if mod:isMother() and room:IsClear() then
+    local invisibleBlock = 1999
+    local cobweb = 1940
+    
+    if entityType == invisibleBlock then
+      table.insert(mod.gridIndexes, gridIdx)
+      return { cobweb, 0, 0 }
+    end
+  end
+end
+
+function mod:onNewRoom()
+  local room = game:GetRoom()
+  
+  for i, v in ipairs(mod.gridIndexes) do
+    mod.gridIndexes[i] = nil
+    room:RemoveGridEntity(v, 0, false)
+  end
+end
+
+function mod:isMother()
+  local level = game:GetLevel()
+  local roomDesc = level:GetCurrentRoomDesc()
+  local stage = level:GetStage()
+  local stageType = level:GetStageType()
+  
+  return not game:IsGreedMode() and
+         (stage == LevelStage.STAGE4_2 or stage == LevelStage.STAGE4_1) and
+         (stageType == StageType.STAGETYPE_REPENTANCE or stageType == StageType.STAGETYPE_REPENTANCE_B) and
+         roomDesc.Data.Shape == RoomShape.ROOMSHAPE_1x2 and
+         roomDesc.Data.Type == RoomType.ROOM_BOSS and
+         roomDesc.Data.Variant == 1 and
+         roomDesc.Data.Name == 'Mother' -- ROOM_SECRET_EXIT_IDX or ROOM_DEBUG_IDX
+end
+
+mod:AddCallback(ModCallbacks.MC_PRE_ROOM_ENTITY_SPAWN, mod.onPreNewRoom)
+mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.onNewRoom)
